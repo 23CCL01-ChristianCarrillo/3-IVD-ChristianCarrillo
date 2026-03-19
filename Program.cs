f5cec7823f6f807b35646def598766ea49a9669b
23CCL01-ChristianCarrillo <ccarrillodb23@gmail.com>
2026-03-19 12:11:44 -0600

    Add files via upload
    Ejercicio en clases

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        //Entrada
+        Console.WriteLine("===Números Positivos o Negativos===");
+        int num;
+        Console.Write("Dame un número entero: ");
+        num = Convert.ToInt32(Console.ReadLine());
+        //Proceso
+        //Salida
+        if (num >= 0)
+        {
+            Console.WriteLine("Es un número positivo");
+        }
+        else 
+        {
+            Console.WriteLine("Es un número negativo");
+        }
+    }
+}
